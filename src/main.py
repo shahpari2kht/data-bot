7d3b29b34034e3661b757aab1f781714e14389fc
Parisa Mohammadzadeh <110891110+shahpari2kht@users.noreply.github.com>
2025-09-21 17:46:31 +0330

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,22 +3,22 @@ from database import save_to_db
 from exporter import export_data
 
 def main():
-input_file = "../data/sample.csv" # مسیر فایل نمونه
-db_file = "../data/data_bot.db" # مسیر دیتابیس
-table_name = "dataset"
-output_file = "../data/cleaned.csv" # مسیر فایل خروجی
+    input_file = "../data/sample.csv" # مسیر فایل نمونه
+    db_file = "../data/data_bot.db" # مسیر دیتابیس
+    table_name = "dataset"
+    output_file = "../data/cleaned.csv" # مسیر فایل خروجی
 
 # 1️⃣ پاک‌سازی داده
-df = clean_data(input_file)
-print("✅ Data cleaned.")
+    df = clean_data(input_file)
+    print("✅ Data cleaned.")
 
 # 2️⃣ ذخیره در دیتابیس
-save_to_db(df, db_file, table_name)
-print(f"✅ Data saved to database: {db_file}")
+    save_to_db(df, db_file, table_name)
+    print(f"✅ Data saved to database: {db_file}")
 
 # 3️⃣ خروجی گرفتن
-export_data(df, output_file, format="csv")
-print(f"✅ Data exported to {output_file}")
+    export_data(df, output_file, format="csv")
+    print(f"✅ Data exported to {output_file}")
 
 if __name__ == "__main__":
 main()
